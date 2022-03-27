8bdd3b7718635f17e1e3c69c1003079e604bf044
Afham <82728352+afham-b@users.noreply.github.com>
2022-03-27 16:33:54 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+
+double payment(double amount_brwd, double interest_rate, int months){
+  double interest = (interest_rate/ 100)/12; 
+  return ((amount_brwd*interest)/(1-pow(1+interest, -months) )); 
+}
+
+int main (){ 
+  double amount_brwd, interest_rate;
+  int months; 
+  cout << "Enter amount borrowed: "; 
+  cin >> amount_brwd; 
+  cout << "Enter interest_rate as percent: ";     
+  cin >> interest_rate;
+  cout << "Enter the number of months the loan is for: ";
+  cin >> months;  
+  cout << "You monthly payment is " << payment(amount_brwd, interest_rate, months) <<endl; 
+  return 0;
+}
